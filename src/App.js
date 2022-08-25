c305f2614fce87b809998b0f42854dea04e1caea
Roberto Padilha <robertocesardasilva@gmail.com>
2022-08-25 20:42:03 -0300

    Aula 25/08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            subOption: 'G1',
+            gradeOption: 'G2',
+            grade: '',
+            subGrade: null,
+        }
+    }
+
+    onSubOptionChanged(event) {
+        const option = event.target.value
+        this.setState({
+            subOption: option,
+            gradeOption: option === 'G1' ? 'G2' : 'G1',
+            grade: '',
+            subGrade: null
+        })
+    }
+
+    onGradeChanged(event) {
+        this.setState({
+            grade: parseFloat(event.target.value)
+        })
+    }
+
+    calculate() {
+        const result = this.state.subOption === 'G1' ?
+            21 - (this.state.grade * 2) :
+            (21 - this.state.grade) / 2
+
+        this.setState({
+            subGrade: result
+        })
+    }
+
+    showResult() {
+        if (this.state.subGrade) {
+            return <b>Sua nota na sub deverá ser {this.state.subGrade}</b>
+        }
+
+        return null
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Calculadora Nota de Sub</h1>
+
+                <label>
+                    <input type="radio" value="G1" checked={this.state.subOption === 'G1'}
+                        onChange={event => this.onSubOptionChanged(event)} /> Sub G1
+                </label>
+                <label>
+                    <input type="radio" value="G2" checked={this.state.subOption === 'G2'}
+                        onChange={event => this.onSubOptionChanged(event)} /> Sub G2
+                </label>
+
+                <br /><br />
+                Nota {this.state.gradeOption}: <br />
+                <input type="number" value={this.state.grade} onChange={event => this.onGradeChanged(event)} />
+
+                <br />
+                <input type="button" value="Calcular" onClick={() => this.calculate()} />
+
+                <br />
+                {this.showResult()}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
